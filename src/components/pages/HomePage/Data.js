c479678e6e7169da70e6746ed1807bd128fe7d5d
Gioele Allievi <stragio.gio@gmail.com>
2021-01-29 20:08:58 +0100

    Initial commit

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Exclusive Access',
+  headline: 'Welcome to LightSoft X',
+  description:
+    'LightSoft X is a tech startup that deals with designing, developing and distributing products for customers. We propose innovative solutions to common problems.',
+  buttonLabel: 'Get Started',
+  // imgStart: '',
+  // img: 'images/svg-1.svg',
+  // alt: 'Credit Card'
+};
+
+export const homeObjTwo = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: '100% SECURE',
+  headline: 'Stay protected 24/7 anywhere anytime',
+  description:
+    'We make sure that our products can guarantee the greatest personal security, using advanced crypting methods.',
+  buttonLabel: 'Learn More',
+  imgStart: '',
+  img: 'images/svg-5.svg',
+  alt: 'Vault'
+};
+
+export const homeObjThree = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'User-Friendly UI',
+  headline: 'Super fast and simple user interface',
+  description:
+    "We design user interfaces that are simple and easy to understand, suitable for everyone and customizable.",
+  buttonLabel: 'Start Now',
+  imgStart: 'start',
+  img: 'images/svg-7.svg',
+  alt: 'Vault'
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'DATA ANALYTICS',
+  headline: 'Every information is stored on your local computer',
+  description:
+    'Never ever have to worry again about data theft. We store your data on your PC memory, so you can access it at any time, even when you are offline!',
+  buttonLabel: 'Download Now',
+  imgStart: 'start',
+  img: 'images/svg-8.svg',
+  alt: 'Vault'
+};
---
+++ src/components/pages/SignUp/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'SIGN UP TODAY',
+  headline: 'Join today to receive an exclusive offer',
+  description:
+    'Get access to our exclusive diamond card that allows you to  send unlimited transactions without getting charged any fees',
+  buttonLabel: 'Sign Up',
+  imgStart: '',
+  img: 'images/svg-6.svg',
+  alt: 'Credit Card'
+};
+
+export const homeObjTwo = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: '100% SECURE',
+  headline: 'Stay protected 24/7 anywhere anytime',
+  description:
+    'We make sure that our products can guarantee the greatest personal security, using advanced crypting methods.',
+  buttonLabel: 'Learn More',
+  imgStart: '',
+  img: 'images/svg-5.svg',
+  alt: 'Vault'
+};
+
+export const homeObjThree = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'User-Friendly UI',
+  headline: 'Super fast and simple user interface',
+  description:
+    "We design user interfaces that are simple and easy to understand, suitable for everyone and customizable.",
+  buttonLabel: 'Start Now',
+  imgStart: 'start',
+  img: 'images/svg-7.svg',
+  alt: 'Vault'
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'DATA ANALYTICS',
+  headline: 'Every information is stored on your local computer',
+  description:
+    'Never ever have to worry again about data theft. We store your data on your PC memory, so you can access it at any time, even when you are offline!',
+  buttonLabel: 'Download Now',
+  imgStart: 'start',
+  img: 'images/svg-8.svg',
+  alt: 'Vault'
+};
